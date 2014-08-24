8b55e6240d7fdfaa1ab0fe548f2aa23772766c88
Omair Majid <omajid@redhat.com>
2014-08-24 00:12:03 -0400

    emacs: don't use customize for backup config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -82,19 +82,10 @@
 ; save and restore previous sessions
 (desktop-save-mode 1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(backup-by-copying t)
- '(backup-directory-alist (quote ((".*" . "~/.saves")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+; backups
+(setq backup-directory-alist '((".*" . "~/.saves"))
+      backup-by-copying t)
+
 
 (use-package autopair
   :ensure
