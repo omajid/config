9e5ef3da761db40c722f2b2c05d248b570673cbe
Omair Majid <omair.majid@gmail.com>
2017-04-02 20:40:32 -0400

    emacs: Use spaceline to get a prettier modeline

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -55,6 +55,12 @@
 
 (use-package diminish)
 
+(use-package spaceline
+  :demand
+  :config
+  (require 'spaceline-config)
+  (spaceline-spacemacs-theme))
+
 ;; use icons instead of text in modeline
 (use-package mode-icons
   :config
